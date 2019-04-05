cd7b71b7d294e6dd89d444e5e1fe2c172c4fd593
julian <julian.gregorio@adhocware.com>
2019-04-05 08:38:59 +0200

    read a file

--- src/test/WebTest.java
+++ src/test/WebTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -11,15 +13,17 @@ import java.net.URL;
  * @author julian
  */
 public class WebTest {
-    
+
     private static final String USER_AGENT = "Mozilla/5.0";
 
     public static void main(String[] args) {
-        
+
         System.out.println(">>>> READ STREAM");
         readWebPageStream("http://web.inslaferreria.cat/");
         System.out.println(">>>> READ CONNECTION");
         readWebPageConnection("http://web.inslaferreria.cat/");
+        System.out.println(">>>> READ FILE");
+        readWebFileStream("http://web.inslaferreria.cat/images/logo_inslaferreria.jpg", "data/laferreria.jpg");
     }
 
     static void readWebPageStream(String urlText) {
@@ -41,34 +45,54 @@ public class WebTest {
 
         }
     }
-    
-    static void readWebPageConnection(String urlText) {
 
+    static void readWebFileStream(String urlText, String fileName) {
+        
         try {
-            URL url = new URL(urlText);
-            HttpURLConnection httpConn = ((HttpURLConnection) url.openConnection());
+            BufferedInputStream in = new BufferedInputStream(new URL(urlText).openStream());
+            FileOutputStream os = new FileOutputStream(fileName);
             
-            httpConn.setRequestMethod("GET"); // optional
-            httpConn.setRequestProperty("User-Agent", USER_AGENT);
-            
-            int responseCode = httpConn.getResponseCode();
-            System.out.println("response code: " + responseCode);
-            
-            String contentType = httpConn.getContentType();
-            System.out.println("content type: " + contentType);
-            
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
-            
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println(inputLine);
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                os.write(dataBuffer, 0, bytesRead);
             }
             
+            os.close();
             in.close();
             
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+    }
+
+    static void readWebPageConnection(String urlText) {
+
+        try {
+            URL url = new URL(urlText);
+            HttpURLConnection httpConn = ((HttpURLConnection) url.openConnection());
+
+            httpConn.setRequestMethod("GET"); // optional
+            httpConn.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = httpConn.getResponseCode();
+            System.out.println("response code: " + responseCode);
+
+            String contentType = httpConn.getContentType();
+            System.out.println("content type: " + contentType);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+
+            in.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
-
